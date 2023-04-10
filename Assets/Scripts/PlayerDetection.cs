e13bdd6b0b9c13ba86ebfd7c945ca902b4ccb72e
JanJangura <111806379+JanJangura@users.noreply.github.com>
2023-04-10 10:31:28 -0400

    YES!

--- Assets/Scripts/PlayerDetection.cs
+++ Assets/Scripts/PlayerDetection.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class PlayerDetection : MonoBehaviour
